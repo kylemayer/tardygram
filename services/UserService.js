e8243dfc27946cc5f21b18366ded7145646f3b63
Brianna Bias <brianna.bias@gmail.com>
2021-08-11 11:55:41 -0700

    user service layer

---
+++ services/UserService.js
@@ -0,0 +1,19 @@
+const User = require('./models/User.js');
+const { exchangeCodeForToken, getUserProfile } = require('./utils/github.js');
+
+module.exports = class UserService {
+  static async create(code) {
+    const token = await exchangeCodeForToken(code);
+    const profile = await getUserProfile(token);
+    const user = await User.findByUser(profile.login);
+
+    if(!user) {
+      return User.insert({
+        username: profile.login,
+        avatarUrl: profile.avatar_url,
+      });
+    } else {
+      return user;
+    }
+  }
+};
