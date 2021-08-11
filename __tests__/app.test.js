410a66ee5488015b24b1b6091020ee1936208ebe
Brianna Bias <brianna.bias@gmail.com>
2021-08-11 14:40:19 -0700

    post image test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,21 +11,17 @@ describe('tardygram routes', () => {
 
 
   it('verify route displays the currently logged in user via GET', async () => {
-
-    console.log('I am running a test');
     const res = await request(app).get('/api/v1/auth/verify');
 
     expect(res.body).toEqual({
-      username: 'test_user',
+      username: 'testuser',
       avatarUrl: 'http://example.com/image.png',
     });
   });
 
   it('creates an image with a caption via POST', async () => {
-    const user = await User.insert({
-      username: 'test_user',
-      avatarURL: 'http://example.com/image.png',
-    });
+    const user = await User.insert('testuser', 'http://example.com/image.png'
+    );
 
     const res = await request(app)
       .post('/api/v1/posts')
@@ -38,12 +34,11 @@ describe('tardygram routes', () => {
 
     expect(res.body).toEqual({
       id: '1',
+      username: 'testuser',
       photo_url: 'http://example.com/image2.png',
       caption: 'image caption',
-      tags: ['sunny', 'summer', 'water']
+      tags: ['sunny', 'summer', 'water'],
     });
   });
-
-
 });
 
