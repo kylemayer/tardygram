87829a0b02087e26834212ae29024eaef7c397c3
Kyle Mayer <x@kylemayer.dev>
2021-08-13 09:27:50 -0700

    most popular posts test route setup started

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -105,7 +105,7 @@ describe('tardygram post routes', () => {
     });
   });
 
-  it.only('updates the caption on a post via PATCH', async () => {
+  it('updates the caption on a post via PATCH', async () => {
     await User.insert('brionlykindasux111', 'http://example.com/image2.png');
     await Post.insert({
       username: 'brionlykindasux111',
@@ -154,5 +154,17 @@ describe('tardygram post routes', () => {
     expect(res.body).toEqual({
       message: 'that junk gone',
     });
+  });
+
+  it('gets the top 10 most popular posts via GET', async () => {
+    await User.insert();
+
+
+
+
+
+
+
+
   });
 });
